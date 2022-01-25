a75da44b49761b658e76dbe96f03887bd9e90250
harsh12chauhan <harsh.chauhan@ssipmt.com>
2022-01-25 14:23:40 +0530

    used classbase components till now

---
+++ src/components/Newsitem.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+export class Newsitem extends Component {
+
+    render() {
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
+        return (
+            <div className='my-3'>
+                <div className="card" >
+                  <div style={{
+                        display:'flex',
+                        justifyContent:'flex-end',
+                        position:'absolute',
+                        right:'0',
+                        margin:' -10px'
+                     } }>
+                    <span className=" badge rounded-pill bg-danger" > {source}</span>
+                    </div>
+                    <img src={!imageUrl ? "https://techcrunch.com/wp-content/uploads/2022/01/christelle-bourgeois-Aq7paIaerrY-unsplash.jpg?w=581" : imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title"> {title}</h5>
+                        <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-muted">By {!author ? "unknown" : author} on {new Date(date).toGMTString()}</small></p>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark"> Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Newsitem
